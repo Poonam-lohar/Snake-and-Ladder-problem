18e6616baf7906747af34d509b73ecf98af47650
Your Name <you@example.com>
2022-11-08 15:59:34 +0530

    added: snake and ladder problem

---
+++ src/com/bridgelabz/GamePlayer.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+import java.util.Random;
+
+public class GamePlayer {
+
+    public static void main(String[] args) {
+
+        System.out.println("starting of Snake and Ladder Game");
+        System.out.println("The single player is at position 0");
+        System.out.println("-------------------------------------");
+    }
+}
+
