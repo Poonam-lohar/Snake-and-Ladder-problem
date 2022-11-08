641e84286be7368d38755735659be242b3732ee9
Your Name <you@example.com>
2022-11-08 16:07:14 +0530

    Modified: Rolls die to get number 0 to 6

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -1,14 +1,15 @@
 package com.bridgelabz;
 
-import java.util.Random;
 
-public class GamePlayer {
+public class SnakeLadder {
 
     public static void main(String[] args) {
 
         System.out.println("starting of Snake and Ladder Game");
         System.out.println("The single player is at position 0");
         System.out.println("-------------------------------------");
+        int dice_roll = (int) (Math.random() * 6 + 1);
+        System.out.println("Dice Rolled :" + dice_roll);
     }
 }
 
